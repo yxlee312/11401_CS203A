7baf4dea2aae7933f5ff8fd86cc9de802ad82bef
Lee Yan Xi <leesteve0312@gmail.com>
2025-11-21 22:20:55 +0800

    Update hash_fn.h

--- AssignmentIV/C/hash_fn.h
+++ AssignmentIV/C/hash_fn.h
@@ -1,15 +1,12 @@
-/*0
+/*
    ========================================
    hash_fn.h — declare your hash functions
    ========================================
-
    Description:
     This file contains the hash functions for integer and string keys.
-
    Development History:
     - 2025/11/17: Initial implementation
-
-   Developer: Yu-Feng Huang <yfhuang@saturn.yzu.edu.tw>
+   Developer: Yan-Xi Lee <leesteve0312@gmail.com>
  */
 #ifndef HASH_FN_H
 #define HASH_FN_H
